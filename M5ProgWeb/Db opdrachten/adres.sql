943b10c174d942cb955cd42a277394842afd0cc9
nazli <n.eroglux014@gmail.com>
2025-10-07 18:30:05 +0200

    woah

---
+++ M5ProgWeb/Db opdrachten/adres.sql
@@ -0,0 +1,6 @@
+USE m5prog;
+
+INSERT INTO adres (straat, huisnummer, postcode, plaatsnaam)
+VALUES
+('Contactweg', '36', '1014AN', 'Amsterdam'),           -- Media College
+('Rozengracht', '114', '1016NB', 'Amsterdam');         -- Restaurant in de buurt
